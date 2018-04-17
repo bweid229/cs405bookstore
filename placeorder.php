f9d59bfe1a7d2f3a8d79a3e7a27b9ecd6e3109da
bweid229 <38406663+bweid229@users.noreply.github.com>
2018-04-16 23:21:09 -0400

    Add files via upload

--- placeorder.php
+++ placeorder.php
@@ -17,29 +17,38 @@ if(!isset($_SESSION))
 
 mysqli_select_db($conn, "bookstore_test4") or die(mysqli_error($conn));         
 
-		echo "<h2>Your Cart</h2>";
-
          if(isset($_SESSION['login_user'])){
 			$userid2 = $_SESSION['login_user'];
-			$results1 = mysqli_query($conn, "SELECT books.`BookID`, `price`, cart.`quantity`, `userID` FROM (books JOIN cart ON books.bookID = cart.bookID) WHERE userID = '$userid2';");                
-			$results2 = mysqli_query($conn, "SELECT books.`BookID`, `price`, cart.`quantity`, `userID` FROM (books JOIN cart ON books.bookID = cart.bookID) WHERE userID = '$userid2';");                
-
+			$results1 = mysqli_query($conn, "SELECT books.BookID AS bookID2, price, how_many, userID FROM (books JOIN cart ON books.bookID = cart.bookID) WHERE userID = '$userid2';");                
+			$results2 = mysqli_query($conn, "SELECT books.BookID AS bookID2, price, how_many, userID FROM (books JOIN cart ON books.bookID = cart.bookID) WHERE userID = '$userid2';");                
 			if(mysqli_num_rows($results1) > 0){
-			while ($row = mysqli_fetch_assoc($results1)){
-			$cost += ($row['price']*$row["cart.`quantity`"]);
-			}
-			}
-			$sql_1 = "INSERT INTO orders ('ord_status', 'cost', 'email', 'billing_address', 'shipping_address', 'credit_card_no', 'date_ordered' ,'userId')
+				while ($row = mysqli_fetch_assoc($results1)){
+					$quantity1 = $row["how_many"];
+					$price = $row["price"];
+					$cost += ($price*$quantity1);
+				}
+		}	
+		echo $row["BookID2"];
+			$sql_1 = "INSERT INTO orders (ord_status, cost, email_address, billing_address, shipping_address, credit_card_no, date_ordered ,userId) VALUES (
 			VALUES ('in process', $cost, $email, $baddress, $saddress, $credit, $date, $userid2)";
 			mysqli_query($conn, $sql_1);
-
+			$last_id = mysqli_insert_id($conn);
+			$get_last = "SELECT MAX(Order_Id) AS max FROM orders";
+			$results3 = mysqli_query($conn, $get_last);
+			$last_id_row = mysqli_fetch_assoc($results3);
+			$last_id = $last_id_row["max"];
 			while ($row = mysqli_fetch_assoc($results2)){
-			$bookiD = $row["BookID"];
-			$quantity = $row["cart.`quantity`"];
-			$orderID = $row["orderID"];
-			$sql_2 = "INSERT INTO  IN_ORDER (BookID, quantity , UserID, orderID )
-				VALUES ($BookID, $quantity, $userid2, $orderID)";
-			mysqli_query($conn, $sql_2);
+			$bookID = $row["bookID2"];
+			$quantity = $row["how_many"];
+			
+			$sql_2 = "INSERT INTO  IN_ORDER (BookID, quantity , order_ID )
+				VALUES ($bookID, $quantity, $last_id)";
+			if (mysqli_query($conn, $sql_2)) {
+					echo "Order placed succesffuly. Thank you!";
+				}			 		
+				else {
+						echo "Error: " . $sql_2 . "<br>" . mysqli_error($conn);
+				}
 			}
 		 }
 ?>		
